553b9126c62f38c0117778712a7bbf5aa004f5f6
tangshuang <mail@tangshuang.net>
2023-08-27 15:56:22 +0800

    fix: merge plugins in web sdk

--- packages/@web/src/index.js
+++ packages/@web/src/index.js
@@ -2,10 +2,11 @@ import { Anys } from 'anys';
 import { AnysRecorderPlugin } from './recorder-plugin.js';
 
 export const create = (options = {}) => new Anys({
-    plugins: {
-        recorder: AnysRecorderPlugin,
-    },
     namespace: 'AnysTracer',
     autoReport: false,
     ...options,
+    plugins: {
+        ...(options.plugins || {}),
+        recorder: AnysRecorderPlugin,
+    },
 });
